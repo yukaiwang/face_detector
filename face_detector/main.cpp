579031cde352f6302fb2bb1e031530d8f2a31cad
yukai <yukai.wang1991@gmail.com>
2015-11-24 11:59:09 +0100

    Initial Commit

---
+++ face_detector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  face_detector
+//
+//  Created by Yukai Wang on 24/11/15.
+//  Copyright © 2015 Yukai Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
